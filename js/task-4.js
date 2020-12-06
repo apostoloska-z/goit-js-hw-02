1edb241852136aa45e2fecfb1da8e0d6290fc17d
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2020-12-06 23:43:22 +0100

    дз

--- js/task-4.js
+++ js/task-4.js
@@ -7,17 +7,17 @@ const formatString = function (string) {
   };
   
   console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-  // вернется оригинальная строка
+
   
   console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-  // вернется форматированная строка
+
   
   console.log(formatString('Curabitur ligula sapien.'));
-  // вернется оригинальная строка
+
   
   console.log(
     formatString(
       'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
     ),
   );
-  // вернется форматированная строка
+  
